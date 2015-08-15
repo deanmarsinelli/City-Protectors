ce6754886478701bbbdeeda45390fa94b6dafaa5
deanmarsinelli <marsinellid@gmail.com>
2015-08-15 10:00:49 -0400

    documentation for point class

--- Cobalt Engine/Source/Include/types.h
+++ Cobalt Engine/Source/Include/types.h
@@ -16,29 +16,54 @@
 class Point
 {
 public:
-	// Constructors
+	/// Default constructor with a (0,0) point
 	Point() { x = y = 0; }
+
+	/// Constructor taking in two coordinates
 	Point(const long _x, const long _y) { x = _x; y = _y; }
+
+	/// Copy constructor from a point reference
 	Point(const Point& newPoint) { x = newPoint.x; y = newPoint.y; }
+
+	/// Copy constructor taking a point pointer
 	Point(const Point* pNewPoint) { x = pNewPoint->x; y = pNewPoint->y; }
 
 	// overloaded operators
-	// assignment
+	/// Assignment operator using a point reference
 	Point& operator=(const Point& newPoint) { x = newPoint.x; y = newPoint.y; return *this; }
+	
+	/// Assignment operator using a point pointer
 	Point& operator=(const Point* pNewPoint) { x = pNewPoint->x; y = pNewPoint->y; return *this; }
-	// addition
+	
+	/// Addition operator
 	Point operator+(const Point& other) { Point temp(this); temp += other; return temp; }
+	
+	/// Addition operator using a point reference
 	Point& operator+=(const Point& other) { x += other.x; y += other.y; }
+	
+	/// Addition assignment using a point pointer
 	Point& operator+=(const Point* pOther) { x += pOther->x; y += pOther->y; }
-	// subtraction
+	
+	/// Subtraction operator
 	Point operator-(const Point& other) { Point temp(this); temp -= other; return temp; }
+	
+	/// Subtraction assignment using a point reference
 	Point& operator-=(const Point& other) { x -= other.x; y -= other.y; }
+	
+	/// Subtraction assignment using a point pointer
 	Point& operator-=(const Point* pOther) { x -= pOther->x; y -= pOther->y; }
-	// comparison
+	
+	/// Comparison operator
 	bool operator==(const Point& other) const { return (x == other.x && y == other.y); }
+	
+	/// Not equal operator
 	bool operator!=(const Point& other) const { return !(x == other.x && y == other.y); }
 
-	long x, y;
+	/// x-coordinate of the point
+	long x;
+
+	/// y-coordinate of the point
+	long y;
 };
 
 /**
