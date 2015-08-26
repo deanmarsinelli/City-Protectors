a2b8490f7493c5fbea7701e49e9242f766420c8c
deanmarsinelli <marsinellid@gmail.com>
2015-08-26 18:07:26 -0400

    support for d3d mesh resources

---
+++ Cobalt Engine/Source/D3DMesh.cpp
@@ -0,0 +1,37 @@
+/*
+	D3DMesh.cpp
+
+	Inspired by Game Coding Complete 4th ed.
+	by Mike McShaffry and David Graham
+*/
+
+#include "D3DMesh.h"
+#include "EngineStd.h"
+#include "Logger.h"
+#include "ResourceHandle.h"
+#include "WindowsApp.h"
+
+bool SdkMeshResourceLoader::LoadResource(char* rawBuffer, unsigned int rawSize, shared_ptr<ResHandle> handle)
+{
+	WindowsApp::Renderer renderer = WindowsApp::GetRendererImpl();
+	if (renderer == WindowsApp::Renderer::Renderer_D3D11)
+	{
+		shared_ptr<D3DSdkMeshResourceExtraData11> extra = shared_ptr<D3DSdkMeshResourceExtraData11>(CB_NEW D3DSdkMeshResourceExtraData11());
+		
+		// load the mesh
+		if (SUCCEEDED(extra->m_Mesh11.Create(DXUTGetD3D11Device(), (BYTE*)rawBuffer, (UINT)rawSize, true)))
+		{
+			handle->SetExtra(shared_ptr<D3DSdkMeshResourceExtraData11>(extra));
+		}
+
+		return true;
+	
+	}
+	else if (renderer == WindowsApp::Renderer::Renderer_D3D9)
+	{
+		CB_ASSERT(0 && "Not Supported in D3D9");
+	}
+
+	CB_ASSERT(0 && "Unsupported renderer in SdkMeshResourceLoader::LoadResource");
+	return false;
+}
---
+++ Cobalt Engine/Source/Include/D3DMesh.h
@@ -0,0 +1,55 @@
+/*
+	D3DMesh.h
+
+	Inspired by Game Coding Complete 4th ed.
+	by Mike McShaffry and David Graham
+*/
+
+#pragma once
+
+#include <SDKmesh.h>
+
+#include "interfaces.h"
+
+/**
+	Extra detail to include with a D3D SDK mesh resource.
+*/
+class D3DSdkMeshResourceExtraData11 : public IResourceExtraData
+{
+	friend class SdkMeshResourceLoader;
+
+public:
+	/// Default constructor
+	D3DSdkMeshResourceExtraData11() { }
+
+	/// Virtual Destructor
+	~D3DSdkMeshResourceExtraData11() { }
+
+	/// Returns a string describing the extra data
+	virtual std::string ToStr() { return "D3DSdkMeshResourceExtraData11"; }
+
+	/// The actual SDK mesh
+	CDXUTSDKMesh m_Mesh11;
+};
+
+/**
+	Resource loader for a D3D SDK mesh
+*/
+class SdkMeshResourceLoader : public IResourceLoader
+{
+public:
+	/// Return the pattern for the mesh loader
+	virtual std::string GetPattern() { return "*.sdkmesh"; }
+
+	/// Return false because the mesh needs extra processing
+	virtual bool UseRawFile() { return false;  }
+
+	/// Return false because we keep the raw mesh data
+	virtual bool DiscardRawBufferAfterLoad() { return false; }
+
+	/// Return the size of the loaded mesh
+	virtual unsigned int GetLoadedResourceSize(char* rawBuffer, unsigned int rawSize) { return rawSize; }
+
+	/// Load a resource into a resource handle
+	virtual bool LoadResource(char* rawBuffer, unsigned int rawSize, shared_ptr<ResHandle> handle);
+};
