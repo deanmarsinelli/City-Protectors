a23c04632cbb3c06da508032506f64a64988e647
deanmarsinelli <marsinellid@gmail.com>
2015-09-14 14:57:44 -0400

    adding a binary network packet class

---
+++ Cobalt Engine/Source/BinaryPacket.cpp
@@ -0,0 +1,62 @@
+/*
+	BinaryPacket.cpp
+
+	Inspired by Game Coding Complete 4th ed.
+	by Mike McShaffry and David Graham
+*/
+
+#include "BinaryPacket.h"
+#include "EngineStd.h"
+#include "Logger.h"
+
+
+BinaryPacket::BinaryPacket(const char* data, u_long size)
+{
+	// construct a packet all at once
+
+	m_Data = CB_NEW char[size + sizeof(u_long)];
+	CB_ASSERT(m_Data);
+	// from MSDN - The htonl function converts a u_long from host 
+	// to TCP/IP network byte order (which is big-endian).
+	*(u_long*)m_Data = htonl(size + sizeof(u_long));
+	memcpy(m_Data + sizeof(u_long), data, size);
+}
+
+BinaryPacket::BinaryPacket(u_long size)
+{
+	// construct an empty packet
+
+	m_Data = CB_NEW char[size + sizeof(u_long)];
+	CB_ASSERT(m_Data);
+	*(u_long*)m_Data = htonl(size + sizeof(u_long));
+}
+
+BinaryPacket::~BinaryPacket()
+{
+	CB_SAFE_DELETE(m_Data);
+}
+
+void BinaryPacket::MemCpy(const char* data, size_t size, int destOffset)
+{
+	// copy data into the packet
+
+	CB_ASSERT(size + destOffset <= GetSize() - sizeof(u_long));
+	memcpy(m_Data + destOffset + sizeof(u_long), data, size);
+}
+	
+const char* BinaryPacket::GetType() const
+{
+	return g_Type;
+}
+
+const char* BinaryPacket::GetData() const
+{
+	return m_Data;
+}
+
+u_long BinaryPacket::GetSize() const
+{
+	// from MSDN - ntohl function converts a u_long from TCP/IP network order 
+	// to host byte order (which is little-endian on Intel processors).
+	return ntohl(*(u_long*)m_Data);
+}
---
+++ Cobalt Engine/Source/Include/BinaryPacket.h
@@ -0,0 +1,47 @@
+/*
+	BinaryPacket.h
+
+	Inspired by Game Coding Complete 4th ed.
+	by Mike McShaffry and David Graham
+*/
+
+#include "interfaces.h"
+
+/**
+	Implementation of a network packet that stores
+	binary data. The packet can be constructed all
+	at once or with calls to MemCpy.
+*/
+class BinaryPacket : public IPacket
+{
+public:
+	/// Construct a packet all at once with data
+	BinaryPacket(const char* data, u_long size);
+
+	/// Construct a packet with a given size
+	BinaryPacket(u_long size);
+
+	/// Virtual destructor frees the packet data
+	virtual ~BinaryPacket();
+
+	/// Copy data into this packet
+	void MemCpy(const char* data, size_t size, int destOffset);
+
+	// IPacket interface
+	/// Return the type of packet
+	virtual const char* GetType() const;
+
+	/// Return the data stored in the packet
+	virtual const char* GetData() const;
+
+	/// Return the size of the packet
+	virtual u_long GetSize() const;
+
+public:
+	/// Type name for binary packets
+	static const char* g_Type;
+
+protected:
+	/// Pointer the data stored in the packet
+	char* m_Data;
+};
