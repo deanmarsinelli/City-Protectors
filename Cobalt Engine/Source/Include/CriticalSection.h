1bdd22e6509ea4919c032569eb928ff8350cf1f1
deanmarsinelli <marsinellid@gmail.com>
2015-10-09 13:34:40 -0400

    scoped critical section class

--- Cobalt Engine/Source/Include/CriticalSection.h
+++ Cobalt Engine/Source/Include/CriticalSection.h
@@ -44,3 +44,43 @@ private:
 	/// The windows CRITICAL_SECTION object
 	mutable CRITICAL_SECTION m_CS;
 };
+
+
+/**
+	This is a helper class that allows automatic locking/unlocking
+	of a critical section when the object is created or destroyed.
+
+	Usage:
+	Create the critical section object
+	Create a scope and add the scoped critical section object
+
+	CriticalSection cs;
+
+	// unlocked code
+
+	{
+		ScopedCriticalSection this_is_locked(cs);
+
+		// code that needs thread locking
+	}
+
+	The scoped critical section will be destroyed at the end of the scope
+	there by unlocking the critical section object.
+*/
+class ScopedCriticalSection
+{
+public:
+	ScopedCriticalSection(CriticalSection& csResource) :
+		m_CSResource(csResource)
+	{
+		m_CSResource.Lock();
+	}
+
+	~ScopedCriticalSection()
+	{
+		m_CSResource.Unlock();
+	}
+
+private:
+	CriticalSection &m_CSResource;
+};
